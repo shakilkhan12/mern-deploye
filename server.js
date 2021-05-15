fe4d6dc50b046d96eafff08ab0a2190ea662ca1f
Shakil Khan <shakilkhan621@gmail.com>
2021-05-15 07:28:28 +0500

    oh great

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use('/', profileRoutes);
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 app.listen(PORT, () => {
 	console.log('Your app is running');
