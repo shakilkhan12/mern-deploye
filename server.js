51e86e6dc329bb543b92c602f33841aabc7277a0
Shakil Khan <shakilkhan621@gmail.com>
2021-05-15 07:34:07 +0500

    upload

--- server.js
+++ server.js
@@ -17,8 +17,12 @@ app.use('/', profileRoutes);
 const PORT = process.env.PORT || 5000;
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running....');
 	});
 }
 app.listen(PORT, () => {
